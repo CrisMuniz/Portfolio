92274feb0c3fc91ef9b1345ee292284595c94c3a
CrisMuniz <cristinamuniz85@gmail.com>
2023-09-25 12:55:09 +0200

    js functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,40 @@
 'use strict';
 
-console.log('>> Ready :)');
+// Llamamos a las clases que necesitamos para la logica del despliegue del menú
+const burger = document.querySelector('.js_burger');
+const list = document.querySelector('.js_links');
+const contact =document.querySelector('.js_contact');
+const portfolio = document.querySelector('.js_portfolio');
+const aboutme = document.querySelector('.js_aboutme');
+
+//Creamos un evento para el icono que nos permite desplegar el menu
+burger.addEventListener('click', handlerClick);
+
+//Funcion manejadora del menú
+function handlerClick() {
+  list.classList.toggle('open');
+  burger.classList.toggle('close');
+
+}
+
+// Creamos los eventos de cada elemento de la lista para trabajar con ellos y las respectivas funciones manejadoras para que se esconda el menu una vez clicamos en cada elemento
+contact.addEventListener('click',handlerClickContact);
+
+function handlerClickContact() {
+  list.classList.toggle('open');
+}
+
+portfolio.addEventListener('click', handlerClickPortfolio);
+
+function handlerClickPortfolio() {
+  list.classList.toggle('open');
+}
+
+aboutme.addEventListener('click', handlerClickAboutme);
+
+function handlerClickAboutme() {
+  list.classList.toggle('open');
+}
+
+
+
